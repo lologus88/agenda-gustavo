ff9fd70e25ae9c8711700a2b1ef1fffee293492e
Gustavo Larreynaga <gustavolarreynaga@Gustavos-MacBook-Pro.local>
2026-04-17 10:55:07 -0500

    Agregar estructura de carpetas

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AgendaDashboard from './agenda-dashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AgendaDashboard />
+  </React.StrictMode>
+);
